dee9b0c8d3cdad340f1a98ec58b7ba5dadb0f1df
Daniel Oertwig <Daniel.Oertwig@googlemail.com>
2013-07-13 00:33:01 +0200

    incremental sensor code updated, USING MM!

--- src/sensor/incremental.c
+++ src/sensor/incremental.c
@@ -5,6 +5,56 @@
 
 
 
+
+//-----------------------------------------------------------------------------
+/** Wheel diameter in mm.
+ * */
+#define DIAMETER_MM 50
+
+
+
+
+/** Pulses per 360 degree rotation.
+ *
+ * TODO
+ * */
+#define PULSE_ROT 24
+
+
+
+
+//-----------------------------------------------------------------------------
+/** Internal structure to store ticks by the sensors.
+ * */
+typedef struct __IncrementalTicks
+{
+	int8_t left;
+	int8_t right;
+} IncrementalTicks;
+
+
+
+
+//-----------------------------------------------------------------------------
+/** History values for both incremental sensors.
+ * */
+static uint8_t incHistory1;
+static uint8_t incHistory2;
+
+
+
+
+/** Variable to store ticks produced by both incremental sensors.
+ * */
+volatile static IncrementalTicks incrementalTicks;
+
+
+
+
+
+//-----------------------------------------------------------------------------
+/** Constructor to setup pins to receive pin change interrupts.
+ * */
 ATTRIBUTE( constructor, used) void Incremental_ctor(void)
 {
 	// Enable pin change interrupt for PCINT0:7
@@ -20,28 +70,54 @@ ATTRIBUTE( constructor, used) void Incremental_ctor(void)
 	incHistory2 = 0;
 }
 
-static uint8_t incHistory1;
-static uint8_t incHistory2;
-
-typedef struct __IncrementalTicks
-{
-	int8_t left;
-	int8_t right;
-} IncrementalTicks;
-
-volatile IncrementalTicks incrementalTicks;
 
 
+
+//-----------------------------------------------------------------------------
+/** Interrupt handler, called whenever one of the 4 pins changes state.
+ *
+ * When the incremental sensor is turned clockwise, it changes its state
+ * like this:
+ * AB ---> AB |
+ * 01      00 | 4
+ * 00      10 | 2
+ * 10      11 | 11
+ * 11      01 | 13
+ *
+ * When turning counter clockwise, the AB pairs from above change places:
+ * AB ---> AB |
+ * 00      01 | 1
+ * 01      11 | 7
+ * 11      10 | 14
+ * 10      00 | 8
+ *
+ * In case A and B are switched, clockwise and counter clockwise are exchanged.
+ *
+ * Timing diagram:
+ * 	---> CW
+ * 	<--- CCW
+ *
+ *      |-OFF-|     |-----
+ * A ---|     |-ON--|
+ *
+ *        |-OFF-|     |----
+ * B -----|     |-ON--|
+ *
+ * */
 ISR(PCINT0_vect)
 {
 	// Read state of pins, TODO: use volatile?
 	uint8_t pinstate = PINB;
+	// Isolate AB of both sensors
 	uint8_t incState1 = pinstate & ((1 << PB4) | (1 << PB5));
-	incState1 >>= PB4;
 	uint8_t incState2 = pinstate & ((1 << PB6) | (1 << PB7));
+	// Shift so that A and B are two least significant bits
+	incState1 >>= PB4;
 	incState2 >>= PB6;
+	// Combine with history
 	uint8_t combined1 = (incHistory1 << 2) | incState1;
 	uint8_t combined2 = (incHistory2 << 2) | incState2;
+	// Update tick information of first sensor
 	switch (combined1)
 	{
 		case 4:
@@ -59,6 +135,7 @@ ISR(PCINT0_vect)
 		default:
 			break;
 	}
+	// Update tick information of second sensor
 	switch (combined2)
 	{
 		case 4:
@@ -76,34 +153,28 @@ ISR(PCINT0_vect)
 		default:
 			break;
 	}
+	// Update history
 	incHistory1 = incState1;
 	incHistory2 = incState2;
 }
 
-typedef struct __WheelDistance_t
-{
-	int8_t left;
-	int8_t right;
-} WheelDistance;
 
 
 
-
+//-----------------------------------------------------------------------------
 WheelDistance Incremental_getDistance(void)
 {
 	IncrementalTicks data;
+	// Protect this area
 	cli();
 	data.left = incrementalTicks.left;
 	data.right = incrementalTicks.right;
+	// Reset ticks to zero!
 	incrementalTicks.left = incrementalTicks.right = 0;
 	sei();
-	WhellDistance distance;
-	distance.left = data.left * (5 * 3.14);
-	distnace.right = data.right * (5 * 3.14);
+	WheelDistance distance;
+	distance.left = (int16_t)data.left * (DIAMETER_MM * 3.14 / PULSE_ROT);
+	distance.right = (int16_t)data.right * (DIAMETER_MM * 3.14 / PULSE_ROT);
 	return distance;
 }
-
-
-
-
 
---
+++ src/sensor/incremental.h
@@ -0,0 +1,32 @@
+#ifndef __SENSOR__INCREMENTAL_H__
+#define __SENSOR__INCREMENTAL_H__ 1
+#include <stdint.h>
+
+
+
+
+//-----------------------------------------------------------------------------
+/** Packed information about traveled distance in mm.
+ * */
+typedef struct __WheelDistance_t
+{
+	int16_t left;
+	int16_t right;
+} WheelDistance;
+
+
+
+
+//-----------------------------------------------------------------------------
+/** Returns traveled distance of both wheels since the last call.
+ *
+ * Should be called at least 1 time per second.
+ * */
+extern WheelDistance Incremental_getDistance(void);
+
+
+
+
+//-----------------------------------------------------------------------------
+#endif
+
